20869d0ee31356121d4074bf40320458f177fd45
cliff.crerar <cliff.crerar@gmail.com>
2019-07-05 03:15:40 +0200

    changed readme.md

--- doczrc.js
+++ doczrc.js
@@ -21,7 +21,8 @@ const doczConfig = {
     },
     files: ['./README.mdx', 'src/**/*.{md,markdown,mdx}'],
     public: '/public',
-    indexHtml: 'public/index.html'
+    indexHtml: 'public/index.html',
+    dest: '/dist'
 };
 
 chLog && console.log(process.env);

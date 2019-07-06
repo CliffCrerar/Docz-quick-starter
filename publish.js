c2b2450e7c72d7890f851827f460f4a3ce52a706
cliff.crerar <cliff.crerar@gmail.com>
2019-07-06 18:18:05 +0200

    auto-commit-on-publish

--- publish.js
+++ publish.js
@@ -2,7 +2,7 @@
  * @ Author: Cliff Crerar
  * @ Create Time: 2019-07-06 13:46:42
  * @ Modified by: Your name
- * @ Modified time: 2019-07-06 16:59:37
+ * @ Modified time: 2019-07-06 18:18:02
  * @ Description: publishes package and pushes the project to master and prod build to gh-pages brach
  */
 
@@ -13,14 +13,14 @@ const fs = require('fs');
 const path = require('path');
 const cp = require('child_process');
 const ghPages = require('gh-pages');
-const deploy = false;
+const release = false;
 
 
 cp.execSync(path.join(__dirname, './git-scr/commit-push.sh auto-commit-on-publish'));
 // deploy ? process.NODE_DEBUG = "gh-pages npm run deploy.js" : console.log('Did not deploy');
 
 publishOptions = {
-    repo: require('./package.json').repository.url,
+    repo: require('./package.json').repository.url.split('+')[1],
     remote: 'upstream',
     message: 'Auto-generated commit',
     user: {
@@ -28,6 +28,7 @@ publishOptions = {
         email: 'cliff.crerar@gmail.com'
     }
 }
+console.log('publishOptions: ', publishOptions);
 
 // console.log(process);
 
@@ -36,9 +37,12 @@ ghPages.publish('dist', publishOptions, onDeployTrue());
 function onDeployTrue(err) {
     err && console.error('ERROR: ', err);
     ut.log('Deploy is true');
-    const cmd = cp.spawn('np', ['--dryrun'], {
-        cwd: process.cwd(),
-        detached: false,
-        stdio: 'inherit'
-    });
+    // const cmd = cp.spawn('np', [ /*'--dryrun'*/ ], {
+    //     cwd: process.cwd(),
+    //     detached: false,
+    //     stdio: 'inherit'
+    // });
+
+    // console.log('cmd: ', cmd);
+    // return cmd;
 }

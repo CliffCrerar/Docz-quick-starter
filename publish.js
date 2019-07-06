57515c2236ec1cbaeb75ce969b32a19c609fe585
cliff.crerar <cliff.crerar@gmail.com>
2019-07-06 16:57:30 +0200

    auto-commit-on-publish

--- publish.js
+++ publish.js
@@ -2,7 +2,7 @@
  * @ Author: Cliff Crerar
  * @ Create Time: 2019-07-06 13:46:42
  * @ Modified by: Your name
- * @ Modified time: 2019-07-06 16:06:47
+ * @ Modified time: 2019-07-06 16:57:27
  * @ Description: publishes package and pushes the project to master and prod build to gh-pages brach
  */
 
@@ -13,9 +13,10 @@ const fs = require('fs');
 const path = require('path');
 const cp = require('child_process');
 const ghPages = require('gh-pages');
-
 const deploy = false;
 
+
+cp.execSync(path.join(__dirname, './git-scr/commit-push.sh auto-commit-on-publish'));
 // deploy ? process.NODE_DEBUG = "gh-pages npm run deploy.js" : console.log('Did not deploy');
 
 publishOptions = {
